8c11f8f53f96bb8b317b810c0a5bda5dbeee6752
Jean Dubois <jd-dev@laposte.net>
2022-02-22 21:48:55 +0100

    larger image + bugfix

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ file = os.path.realpath(str(input("Enter image path: ")))
 final_file_path = os.path.realpath(str(input("Enter final file path: ")))
 
 if os.path.exists(final_file_path):
-    erase = str(input(f"Erase {os.path.normpath(final_file_path)} ? (Y/N) :"))
+    erase = str(input(f"Erase {os.path.normpath(final_file_path)} ? (Y/N): "))
     erase_ = True if erase.upper() == "Y" else False
     while not erase_:
         final_file_path = os.path.realpath(str(input("Enter new final file path: ")))
         if os.path.exists(final_file_path):
-            erase = str(input(f"Erase {os.path.normpath(final_file_path)} ? (Y/N) :"))
+            erase = str(input(f"Erase {os.path.normpath(final_file_path)} ? (Y/N): "))
             erase_ = True if erase.upper() == "Y" else False
         else:
             erase_ = True  # the file does not exist ;)
@@ -21,16 +21,18 @@ ascii_char = ' .:-=+*#%@'
 
 with Image.open(file) as image:
     # cross product
-    # x  50
+    # x  100
     # y  y'
-    # y' = 50y//x
-    image = image.resize((50, (50 * image.height // image.width)))
+    # y' = 100y//x
+    image = image.resize((100, (100 * image.height // image.width)))
     with open(final_file_path, "w", encoding="UTF-8") as final_file:
         for y in range(image.height):
             line = ''
             for x in range(image.width):
                 # get pixel in RGB format
                 pixel_rgb = image.getpixel((x, y))
+                if isinstance(pixel_rgb, int):
+                    pixel_rgb = [pixel_rgb, pixel_rgb, pixel_rgb]
                 # grey level (between 0 and 255) is obtained by calculating the mean of each RGB color
                 # (between 0 and 255 too)
                 grey = sum(pixel_rgb) // len(pixel_rgb)
